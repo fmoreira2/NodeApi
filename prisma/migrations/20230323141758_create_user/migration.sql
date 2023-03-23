4d49ad5ee85e68f0f6ff17d9521f305f5f817ac1
Fernando Moreira <fernando.m.silva@outlook.com>
2023-03-23 11:44:49 -0300

    apiNodejs - Primeiro Teste

---
+++ prisma/migrations/20230323141758_create_user/migration.sql
@@ -0,0 +1,6 @@
+-- CreateTable
+CREATE TABLE "usuarios" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "nome" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL
+);
---
+++ server/src/server.ts
@@ -0,0 +1,31 @@
+//Back-end api rest
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+app.register(cors)
+
+//lista retornando usuarios filtrados
+app.get('/usuariosF', async () => {
+    const usuarios = await prisma.usuario.findMany({
+        where: {
+            nome:{
+                startsWith: 'Fe'
+            }
+        }
+    })
+    return usuarios
+})
+
+//lista de retornando todos os usuários
+app.get("/usuarios", async () => {
+  const usuarios = await prisma.usuario.findMany();
+  return usuarios;
+});
+
+app.listen({
+    port: 3333
+})
